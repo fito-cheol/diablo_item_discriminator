d1ae4df98ac687dbd2ca7b1229896b7526b7af40
DESKTOP-B5JS4T8\aufe <dmsghs92@gmail.com>
2023-06-28 12:43:13 +0900

    Remove Gray Option

--- main.py
+++ main.py
@@ -20,20 +20,22 @@ def mark_4_corner(image_path):
     DOWN_RIGHT = "ref_image/down_right.png"
 
     image_manager = ImageManager(image_path)
-    image_manager, *_ = image_manager.image_finder(UP_LEFT, use_gray=False)
+    image_manager, *_ = image_manager.image_finder(UP_LEFT)
     image_manager.save_image(path1)
 
-    image_manager, *_ = image_manager.image_finder(DOWN_LEFT, use_gray=False)
+    image_manager, *_ = image_manager.image_finder(DOWN_LEFT)
     image_manager.save_image(path2)
 
-    image_manager, *_ = image_manager.image_finder(UP_RIGHT, use_gray=False)
+    image_manager, *_ = image_manager.image_finder(UP_RIGHT)
     image_manager.save_image(path3)
 
-    image_manager, *_ = image_manager.image_finder(DOWN_RIGHT, use_gray=False)
+    image_manager, *_ = image_manager.image_finder(DOWN_RIGHT)
     image_manager.save_image(path4)
 
 if __name__ == '__main__':
     mark_4_corner(FULL_SCREEN)
+
+    
     # image_with_rect = ItemFinder().mark_item_box(FULL_SCREEN)
     # show_image(image_with_rect)
     #
@@ -45,6 +47,7 @@ if __name__ == '__main__':
     # show_image(image_with_rect)
     # image_with_rect = ItemFinder().mark_item_box(SCREEN_UNIQUE)
     # show_image(image_with_rect)
+
 
 
 
